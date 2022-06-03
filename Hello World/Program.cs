1bc7b6ef0da90f2ba5abe4c06c6e3a54fdc568ec
sruthith <sruthithch@gmail.com>
2022-06-03 15:24:40 -0400

    Added ReadLine

--- Hello World/Program.cs
+++ Hello World/Program.cs
@@ -7,6 +7,9 @@ namespace Hello_World
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.ReadLine();
         }
+        
     }
+    
 }
